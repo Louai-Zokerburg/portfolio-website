2f645538f27fe6984f476daba43e6bc769627bcb
Louai Dourov <92324961+Louai-Zokerburg@users.noreply.github.com>
2023-12-16 21:00:04 -0800

    adding the cover image

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,18 +1,57 @@
-import Orbits from '@/components/shared/orbits'
-import PageTitle from '@/components/shared/page-title'
 import Link from 'next/link'
 import React from 'react'
+
+import Orbits from '@/components/shared/orbits'
+import PageTitle from '@/components/shared/page-title'
+
+import { Button } from '@/components/ui/button'
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import {
+    Tabs,
+    TabsContent,
+    TabsList,
+    TabsTrigger,
+} from "@/components/ui/tabs"
 
 const Projects = () => {
     return (
         <section className='relative' >
 
             <section className=' z-10 relative container flex flex-col justify-center items-start'>
-            <PageTitle title='My Reccent Projects' />
+                <PageTitle title='My Reccent Projects' />
 
-            <article className='w-full px-4 lg:px-8 py-8 rounded-t-2xl mt-8 bg-secondary-color-light dark:bg-secondary-color-dark'>
-                <Link href='projects/12'>Hello</Link>
-            </article>                
+                <article className='w-full px-4 lg:px-8 py-8 rounded-t-2xl mt-8 bg-secondary-color-light dark:bg-secondary-color-dark'>
+                    <Tabs defaultValue="account" className="w-full">
+                        <TabsList className="grid w-full grid-cols-4">
+                            <TabsTrigger value="all">All projects</TabsTrigger>
+                            <TabsTrigger value="mern">MERN projects</TabsTrigger>
+                            <TabsTrigger value="next.js">Next.js projects</TabsTrigger>
+                            <TabsTrigger value="apis">Backend APIs</TabsTrigger>
+                        </TabsList>
+
+                        <TabsContent value="all">
+                            <h3>All Projects</h3>
+                        </TabsContent>
+                        <TabsContent value="mern">
+                            <h3>MERN Projects</h3>
+                        </TabsContent>
+                        <TabsContent value="next.js">
+                            <h3>Next.js Projects</h3>
+                        </TabsContent>
+                        <TabsContent value="apis">
+                            <h3>Backend APIs</h3>
+                        </TabsContent>
+                    </Tabs>
+                </article>
             </section>
 
             <Orbits mini={true} />
--- src/components/ui/tabs.tsx
+++ src/components/ui/tabs.tsx
@@ -14,7 +14,7 @@ const TabsList = React.forwardRef<
   <TabsPrimitive.List
     ref={ref}
     className={cn(
-      "inline-flex h-10 items-center justify-center rounded-md bg-slate-100 p-1 text-slate-500 dark:bg-slate-800 dark:text-slate-400",
+      "inline-flex h-12 items-center justify-center rounded-md bg-accent-color-light p-2 dark:bg-accent-color-dark",
       className
     )}
     {...props}
@@ -29,7 +29,7 @@ const TabsTrigger = React.forwardRef<
   <TabsPrimitive.Trigger
     ref={ref}
     className={cn(
-      "inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium ring-offset-white transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-white data-[state=active]:text-slate-950 data-[state=active]:shadow-sm dark:ring-offset-slate-950 dark:focus-visible:ring-slate-300 dark:data-[state=active]:bg-slate-950 dark:data-[state=active]:text-slate-50",
+      "inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium ring-offset-white transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-primary-color data-[state=active]:text-black-color data-[state=active]:font-semibold dark:ring-offset-slate-950 dark:focus-visible:ring-slate-300",
       className
     )}
     {...props}
